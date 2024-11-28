e727aef3f9590692b93f9ace3c121e7c550ba661
anjalikesherwani <122610979+anjalikesherwani@users.noreply.github.com>
2024-11-28 17:00:31 +0530

    first commit

---
+++ project-1/src/App.jsx
@@ -0,0 +1,14 @@
+const App = () => {
+    return <div>
+        <nav>
+            <div className="logo">
+                <img src="logo.png" alt="Logo"/> 
+            </div>
+        </nav>
+    </div>;
+
+
+
+};
+
+export default App;
